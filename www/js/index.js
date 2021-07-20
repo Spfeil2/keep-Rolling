c4d238c07f8db538786cf7cc62c29c645df485d1
Spfeil2 <simon_pfeil96@yahoo.de>
2021-07-20 16:14:03 +0200

    update

--- www/js/index.js
+++ www/js/index.js
@@ -368,3 +368,20 @@ map.on('dragend',function(e){
         isGeolocationActive = false;
     } 
 });
+
+
+
+const foto = () => {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 25,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+    
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+    
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
